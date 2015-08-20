497bec2d8481a99358e0e78e9a99087f2cebefcb
shyshi <shyshyshy@outlook.com>
2015-08-20 09:51:22 +0800

    reWrite the Whole Script with clear Varabile Names
    重新用Python编写整个程序。给予每个变量清晰的名称以体现其内容。目前具备处理USRBIN、TRACK和BDX的能力。未来可根据需求加入各种处理命令即可

---
+++ test1.py
@@ -0,0 +1,88 @@
+import os
+
+def execute():
+        times=raw_input("Please input the time you want to cycle:")
+        runcommand="$FLUPRO/flutil/rfluka -N0 -M"+str(times)+" input.inp"
+        os.system(runcommand)
+
+def makeusrfiles(usrcard,cardtype):
+        f1=open("input.inp")
+        count_of_lines=0
+        usrcards=["a"]
+        detectornames=["a"]
+        index_of_usrcards=["a"]
+        for lines in f1.readlines():
+                if lines.startswith(usrcard):
+                        usrcards.append(lines)
+                        count_of_lines+=1
+        print(count_of_lines)
+        if (count_of_lines!=0):
+                count_of_cards=1
+                while (count_of_cards<count_of_lines):
+                        temp=usrcards[count_of_cards]
+                        detectorname_in_cards=temp[70:80]
+                        print(detectorname_in_cards)
+                        print(temp)
+                        count_of_cards+=2
+                        temp_split=detectorname_in_cards[0:len(detectorname_in_cards)-1]
+                        print(temp_split)
+                        detectornames.append(temp_split)
+                        usrcardsindexline=temp.split()
+                        index=usrcardsindexline[3][1:len(usrcardsindexline[3])-2]
+                        index_of_usrcards.append("."+index)
+                print(detectornames)
+                detectornames.remove("a")
+                print(index_of_usrcards)
+                index_of_usrcards.remove("a")
+                print(index_of_usrcards)
+                number_of_cards=len(detectornames)
+                print(count_of_cards)
+                rootdir=os.getcwd()
+                count_of_cards=0
+                while (count_of_cards<number_of_cards):
+                        FileList=[]
+                        for root, subFolders, files in os.walk(rootdir):
+                                for f in files:
+                                        if (f.find(index_of_usrcards[count_of_cards])!=-1):
+                                                FileList.append(f)
+                        print(FileList)
+                        ScriptsName=usrcard+"Script"+str(count_of_cards)
+                        tempfile=open(ScriptsName,"w")
+                        tempfile.close()
+                        tempfile=open(ScriptsName,"r+")
+                        tempfile.write("#!/bin/bash\n")
+                        tempfile.write("\n")
+                        tempfile.write("$FLUPRO/flutil/us"+cardtype+"suw <<EOF\n")
+                        for item in FileList:
+                                tempfile.writelines(item+'\n')
+                        detectorname=name+detectornames[count_of_cards]+index_of_usrcards[count_of_cards][1:3]
+                        tempfile.write("\n")
+                        tempfile.write(detectorname+"\n")
+                        tempfile.write("EOF")
+                        tempfile.close()
+                        os.system("chmod u+x "+ScriptsName)
+                        os.system("./"+ScriptsName)
+                        if (cardtype=="b"):
+                                ScriptsName="USBREAScript"+str(count_of_cards)
+                                tempfile=open(ScriptsName,"w")
+                                tempfile.write("#!/bin/bash\n")
+                                tempfile.write("\n")
+                                tempfile.write("$FLUPRO/flutil/usbrea <<EOF\n")
+                                tempfile.write(detectorname+"\n")
+                                tempfile.write(detectorname+".usrbin\n")
+                                tempfile.write("EOF")
+                                tempfile.close()
+                                os.system("chmod u+x "+ScriptsName)
+                                os.system("./"+ScriptsName)
+                        count_of_cards+=1
+
+def afterrun():
+        os.system("rm -rf US*")
+        os.systen("rm -rf ran*")
+
+name=raw_input("Please make sure that you run the fluka script in screen shell.\n Please input the file name you want:")
+execute()
+makeusrfiles("USRTRACK","t")
+makeusrfiles("USRBIN","b")
+makeusrfiles("USRBDX","x")
+afterrun()
