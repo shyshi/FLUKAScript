0b257059d96e00e42b73957de6d8894ab4ab2424
Shy Shi <shyshyshy73@gmail.com>
2016-07-09 20:22:19 +0800

    Debug

--- randomize.py
+++ randomize.py
@@ -1,8 +1,8 @@
 import os
-import commands
+import subprocess as sp
 
 def check(runtime):
-    topresult=commands.getoutput("top -b -n 1 | grep flukahp")
+    topresult=sp.getoutput("top -b -n 1 | grep flukahp")
     string="\n"
     current=topresult.count(string)
     current=current+1
@@ -11,27 +11,52 @@ def check(runtime):
         cores=runtime
         randomize(cores)
     else:
-        cores=capacity;
+        cores=capacity
         randomize(cores)
-        runtime=runtime-capacity
-        check(runtime)
+        nexttime=runtime-capacity
+        check(nexttime)
 
 def randomize(cores):
-    finput=open("input.inp")
     current=1
     while (current <= cores):
+        finput=open("input.inp")
         if (current >= 10):
             seed = str(current) + str(current) + str(current)
         else:
             seed = str(current) + str(current) + str(current) + str(current) + str(current) + str(current)
-        lines=finput.readlines();
-        randomline="RANDOMIZ         1.0   "+seed+"."
-        for line in lines():
-            if line.startswith("RANDOMIZE"):
+        lines=finput.readlines()
+        randomline="RANDOMIZ         1.0   "+seed+".\n"
+        for line in lines:
+            if line.startswith("RANDOMIZ"):
                 number=lines.index(line)
                 lines[number]=randomline
+                print(lines[number])
         randominputfile="input"+str(current)+".inp"
         frandominput=open(randominputfile,"w")
-        for line in lines():
+        for line in lines:
             frandominput.write(line)
         current=current+1
+        finput.close()
+        frandominput.close()
+
+check(12)
+#
+# finput=open("input.inp")
+# current=1
+# cores = 5
+# while (current <= cores):
+#     if (current >= 10):
+#         seed = str(current) + str(current) + str(current)
+#     else:
+#         seed = str(current) + str(current) + str(current) + str(current) + str(current) + str(current)
+#     lines=finput.readlines()
+#     randomline="RANDOMIZ         1.0   "+seed+".\\n"
+#     for line in lines:
+#         if line.startswith("RANDOMIZE"):
+#             number=lines.index(line)
+#             lines[number]=randomline
+#     randominputfile="input"+str(current)+".inp"
+#     frandominput=open(randominputfile,"w")
+#     for line in lines:
+#         frandominput.write(line)
+#     current=current+1
