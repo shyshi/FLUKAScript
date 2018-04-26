5d67fe7e95167d859b8926caa17f24aafbecfdbf
Haoyu Shi <shyshi@shyshi.work>
2018-04-26 18:53:19 +0800

    Add Script.py
    with Python 2.7
    can run and process files automatically
    can run with several subprocesses

--- script.py
+++ script.py
@@ -9,14 +9,13 @@ def findfile():  # List and Let user choose the input file to run
                         if f.endswith("inp"):
                                 fileLists.append(f)
         fileCount=len(fileLists)
-        print(fileCount)
         if (fileCount==0):
                 print("No Input File Found!")
         else:
                 for f in fileLists:
                         findex=fileLists.index(f)+1
                         print(str(findex)+" "+f)
-                selectFile=input("Please input the number of the input file you with to execute\n")
+                selectFile=raw_input("Please input the number of the input file you with to execute\n")
                 selectFile=int(selectFile)-1
                 fileName=fileLists[selectFile]
         return fileName
@@ -29,7 +28,7 @@ def changerandom(index,inputfile):
                 if content.startswith("RANDOMIZ"):
                         lineindex=contents.index(content)
                         randomnumber=int(random.random()*1E6)
-                        newcontent="RANDOMIZ     "+str(randomnumber)+".\n"
+                        newcontent="RANDOMIZ          1.   "+str(randomnumber)+".\n"
                         contents[lineindex]=newcontent
         fileMainName=inputfile.rstrip("inp")
         fileMainName=fileMainName.rstrip(".")
@@ -41,20 +40,46 @@ def changerandom(index,inputfile):
         workFile.close()
 
 def generateFiles(inputfile):
-        subProcessNumber=input("Please input the number of subprocesses you want to run\n")
+        subProcessNumber=raw_input("Please input the number of subprocesses you want to run\n")
         for i in range(0,int(subProcessNumber)):
                 changerandom(i+1,inputfile)
 
 def execute():
-        userroutingCheck=input("Please input Y if you have user routing file:\n")
-        if (userroutingCheck=="Y"):
-                userroutingFile=input("Please input the user routing file name:\n")
-                runcommandpreifx="$FLUPRO/flutil/rfluka -e "+userroutingFile+" -N0 -M5 "
+        rootdir=os.getcwd()
+        userroutingFileLists=[]
+        for root, subFolders, files in os.walk(rootdir):
+                for f in files:
+                        if f.endswith("inp"):
+                                continue
+                        else:
+                                userroutingFileLists.append(f)
+        fileCount=len(userroutingFileLists)
+        if (fileCount==0):
+                print("No File Found!")
         else:
+                for f in userroutingFileLists:
+                        findex=userroutingFileLists.index(f)+1
+                        print(str(findex)+" "+f)
+                selectFile=raw_input("Please input the index of the user routing file you with to execute, 0 for\n")    
+        if (selectFile=="0"):
                 runcommandpreifx="$FLUPRO/flutil/rfluka -N0 -M5 "
+        else:
+                selectFile=int(selectFile)-1
+                fileName=userroutingFileLists[selectFile]
+                runcommandpreifx="$FLUPRO/flutil/rfluka -e "+fileName+"-N0 -M5 "
         for everyfile in inputfileLists:
-                runcommand=runcommandpreifx+everyfile
-                #os.system(runcommand)
+                runcommand=runcommandpreifx+everyfile+"&"
+                os.system(runcommand)
+
+def checkflukafolder():
+    subfoloderList=[]
+    rootdir=os.getcwd()
+    for root, subFolders, files in os.walk(rootdir):
+        for subFolder in subFolders:
+            if subFolder.startswith("fluka"):
+                subfoloderList.append(subFolder)
+    foldernumber=len(subfoloderList)
+    return foldernumber
 
 def makeusrfiles(usrcard,cardtype,inputfile):
         f1=open(inputfile)
@@ -127,21 +152,30 @@ def makeusrfiles(usrcard,cardtype,inputfile):
                                 os.system("./"+ScriptsName)
                         count_of_cards+=1
 
-def afterrun():
+def afterrun(inputfile):
         for everyfile in inputfileLists:
                 os.system("rm -rf "+everyfile)
         os.system("rm -rf US*")
         os.system("rm -rf ran*")
+        os.system("mv "+inputfile+" input.inp")
+        prefix=inputfile.rstrip(".inp")
+        os.system("rm -rf "+prefix+"*")
+        os.system("mv input.inp "+inputfile)
         finishcommand="echo \""+name+" is Finished.\" | mail -s \"Mission Complete\" shihy@ihep.ac.cn"
         os.system(finishcommand)
 
 inputfile=findfile()
-name=input("Please input the name you want:\n")
+name=raw_input("Please input the name you want:\n")
 print(inputfile)
 inputfileLists=[]
 generateFiles(inputfile)
 execute()
+os.system("sleep 10")
+checked=checkflukafolder()
+while (checked!=0):
+    os.system("sleep 5")
+    checked=checkflukafolder()
 makeusrfiles("USRTRACK","t",inputfile)
 makeusrfiles("USRBIN","b",inputfile)
 makeusrfiles("USRBDX","x",inputfile)
-afterrun()
+afterrun(inputfile)
